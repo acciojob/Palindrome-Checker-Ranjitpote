230d8b8e9edad59f03e33294dafacaff824eb4ab
Ranjit Pote <116176451+Ranjitpote@users.noreply.github.com>
2023-01-11 09:30:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+function isPalindrome(string) {
+    // Convert the string to lowercase
+    string = string.toLowerCase();
+    // Reverse the string
+    let reversedString = string.split('').reverse().join('');
+    // Compare the reversed string to the original string
+    if (string === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 }
 module.exports = palindrome
